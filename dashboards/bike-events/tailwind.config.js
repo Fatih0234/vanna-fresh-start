447d3ddf791c05171b47295a62ea0044bad25173
fatihkarahan <sekanti02@gmail.com>
2026-02-05 01:25:39 +0100

    Define primary Tailwind palette for dashboard buttons

--- dashboards/bike-events/tailwind.config.js
+++ dashboards/bike-events/tailwind.config.js
@@ -2,7 +2,22 @@ export default {
   content: ['./src/**/*.{ts,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          50: '#fdf7f0',
+          100: '#f9ecdd',
+          200: '#f0d6b4',
+          300: '#e6be8b',
+          400: '#dba56a',
+          500: '#c88644',
+          600: '#8b5a2b',
+          700: '#7a4f26',
+          800: '#6a4422',
+          900: '#56361a',
+        },
+      },
+    },
   },
   plugins: []
 }
